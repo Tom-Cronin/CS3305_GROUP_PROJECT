11cbde9addf896b49ffb64449319f10cf4f04ce9
Tom <tom100cronin@gmail.com>
2020-01-25 19:08:54 +0000

    added a class with a few methods

--- manage_levels.py
+++ manage_levels.py
@@ -6,7 +6,7 @@ import pygame as pygame
 pygame.init()
 screen_height = 640
 screen_width = 800
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 white = (255, 255, 255)
 
 
@@ -26,4 +26,37 @@ def run_game():
         current_node = draw_map(list_of_nodes, current_node)
 
 
-run_game()
+#run_game()
+
+#
+class map(object):
+    def __init__(self, seed, level):
+        self.seed = seed
+        self.originalSeed = seed
+        self.level = level
+
+        self.currentNode = 0
+
+    def draw_map(self, list_of_nodes, node):
+        current_node = draw_level_map(list_of_nodes, screen_width, screen_height, screen, node)
+        return current_node
+    def createScreen(self):
+        pygame.init()
+        screen_height = 640
+        screen_width = 800
+        screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
+        white = (255, 255, 255)
+        screen.fill(white)
+        pygame.display.update()
+        list_of_nodes = generate_map_list(self.level)
+        length_of_level = len(list_of_nodes) - 1
+        for i in range(length_of_level):
+            self.currentNode = self.draw_map(list_of_nodes, self.currentNode)
+
+    def returnSeedNew(self):
+        return self.seed
+
+    def returnSeed(self):
+        return self.originalSeed
+myMap = map(10, 10)
+myMap.createScreen()
