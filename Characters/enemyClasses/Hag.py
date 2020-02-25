44e9290420fdfa3d8c30b80800d6aeb9770a4fce
Tom <32239311+FumoDraconis@users.noreply.github.com>
2020-02-25 15:05:06 +0000

    Update Hag attack to correct spelling

--- Characters/enemyClasses/Hag.py
+++ Characters/enemyClasses/Hag.py
@@ -18,7 +18,7 @@ class Hag(Character):
 
         self.attack_slot_1 = lifeDrain.LifeDrain(self.intelligence)
         self.attack_slot_2 = Longbow.Longbow(self.dexterity)
-        self.attack_slot_3 = LongSwordStrike.onoHanded(self.strength)
+        self.attack_slot_3 = LongSwordStrike.oneHanded(self.strength)
         self.attack_slot_4 = LongSwordStrike.twoHanded(self.strength)
 
         self.allAttacks = [self.attack_slot_1, self.attack_slot_2,self.attack_slot_3, self.attack_slot_4]
