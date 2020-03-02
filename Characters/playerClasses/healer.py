236540b05be2b2613dc4dfd464bbe20c1d6f4f35
tom <tom100cronin@gmail.com>
2020-03-02 22:29:59 +0000

    changed loading screen to between rooms only

--- Characters/playerClasses/healer.py
+++ Characters/playerClasses/healer.py
@@ -27,10 +27,7 @@ class Healer(Character):
         self.attack_slot_3 = NaturesTouch(self.intelligence)
         self.attack_slot_4 = TendingWounds(self.intelligence)
 
-        self.attack_slot_4.name = "Demo Kill"
-        self.attack_slot_4.baseDamage = 99999
-        self.attack_slot_4.coolDown = 0
-        self.attack_slot_4.isHeal = False
+
 
 
         self.allAttacks = [self.attack_slot_1, self.attack_slot_2, self.attack_slot_3, self.attack_slot_4]
