b070f88d52c912070998000bce0d419e95febc69
IanMontes <37030281+IanMontes@users.noreply.github.com>
2020-03-06 12:41:56 +0000

    Commented healingRoom and LoadingScreen

--- Stages/healingRoom.py
+++ Stages/healingRoom.py
@@ -1,13 +1,11 @@
 
 from Stages.baseStageClass import *
 import time
-from textInput import  TextInput
-
-
 
 class HealStage():
 
     def __init__(self, screen,team):
+        # initializes screen with font and screen details
         self.team = team
         self.font = 'Stages/media/Chapaza.ttf'
         self.fontsize = 20
@@ -15,8 +13,8 @@ class HealStage():
         self.baseScreen.bgImage = pygame.transform.scale(pygame.image.load('Stages/media/MainMenueBackground.png').convert(),
                                                          (self.baseScreen.screen_height, self.baseScreen.screen_width))
 
-
     def display(self):
+        # displays the background image as well as all the text for the healing room.
         self.baseScreen.display.blit(self.baseScreen.bgImage, (0, 0))
         font = pygame.font.Font(self.font, self.fontsize)
         name = font.render("You take time to rest and recover", True, (0,0,0))
@@ -30,22 +28,24 @@ class HealStage():
         pygame.display.update()
 
     def restoreHealth(self):
+        # restores all health for everyone in the team
         for ally in self.team:
             ally.health = ally.maxHealth
         return False
 
-
     def mainLoop(self):  # listens for events
         self.display()
         mainLoop = True
-        while (mainLoop):
+        while mainLoop:
             mainLoop = self.restoreHealth()
             time.sleep(3)
 
-if __name__=="__main__":
-        baseScreen = BaseStage(1300, 700)
-        pygame.init()
-        pygame.mixer.init()
-        myStage =HealStage(baseScreen, [])
-        myStage.mainLoop()
-        pygame.quit()
+
+if __name__ == "__main__":
+    # testing purposes.
+    baseScreen = BaseStage(1300, 700)
+    pygame.init()
+    pygame.mixer.init()
+    myStage = HealStage(baseScreen, [])
+    myStage.mainLoop()
+    pygame.quit()
